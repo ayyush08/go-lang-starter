4f268bf8c46ed5a1b36f01dede25ad66e4cc17e8
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-02-18 22:24:58 +0530

    Post request

--- server/main.go
+++ server/main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	PerformGetRequest()
-	
+	// PerformGetRequest()
+	PerformPostRequest()
 }
 
 func PerformGetRequest() {
@@ -43,5 +43,35 @@ func PerformGetRequest() {
 	fmt.Println("Bytes written to responseString:", byteCount)
 	fmt.Println(responseString.String()) //string here is a method, it converts the raw or byteform data to a string. Earlier string worked and both are good but this is more efficient and recommended as it comes with more features
 
+
+}
+
+
+func PerformPostRequest() {
+	const myurl = "http://localhost:5000/postform"
+
+	//fake json payload
+
+	reqBody := strings.NewReader(`
+	{
+	"name":"Ichigo Kurosaki",
+	"age":20,
+	"job":"Soul Reaper"
+	}`) // we can create any kind of data inside these backticks
+
+	res,err := http.Post(myurl, "application/json",reqBody)
+
+	if(err != nil){
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	fmt.Println("Response status:", res.StatusCode)
+	fmt.Println("Response content length:", res.ContentLength)
+
+	content, _ := io.ReadAll(res.Body)
+
+	fmt.Println("Response body:", string(content))
 
 }
