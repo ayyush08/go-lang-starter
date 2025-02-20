5aefbaa4b0d0d91613bb0d80cd00e2d2ad4ee13c
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-02-20 16:37:40 +0530

    get course by id

--- 24API/main.go
+++ 24API/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 //Model for course - file (supposed to go in a file)
@@ -47,5 +49,26 @@ func getAllCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses) //whatever we write inside Encode will be treated as json and will be thrown back to the person requesting
-	
+
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+	//grab the id from the url or request
+
+	params := mux.Vars(r) //grabbing the parameters from the request
+
+	fmt.Println("Params: ", params)
+
+	//loop through the courses and find the course with the id
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("Course not found with the id " + params["id"])
+	return
+
 }
